6de9082000e5fdc038210a2acca133e6dc96eef3
Cássio Zen <cassiozen@gmail.com>
2017-03-01 22:02:35 -0800

    Added exercises

---
+++ immutable.test.js
@@ -0,0 +1,197 @@
+describe('Immutable manipulations for Arrays', () => {
+  test('Array Shallow copy', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Apples'];
+    /* 
+     * Use the spread operaror to make a shallow copy of the fruitBasket array.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+    expect(updatedFruitBasket).toEqual(['Oranges', 'Bananas', 'Apples']);
+    expect(updatedFruitBasket).not.toBe(fruitBasket);
+  })
+
+  test('Immutable Append', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Apples'];
+    /* 
+     * Use the spread operaror to make a shallow copy of the fruitBasket array 
+     * while at the same time adding a new item "Melon" to the end of the array.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+    expect(updatedFruitBasket).toEqual(['Oranges', 'Bananas', 'Apples', 'Melon']);
+    expect(updatedFruitBasket).not.toBe(fruitBasket);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Apples']);
+  })
+
+  test('Immutable Prepend', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Apples'];
+    /* 
+     * Use the spread operaror to make a shallow copy of the fruitBasket array 
+     * while at the same time adding a new item "Pineapple" to the beginning of the array.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+    expect(updatedFruitBasket).toEqual(['Pineapple', 'Oranges', 'Bananas', 'Apples']);
+    expect(updatedFruitBasket).not.toBe(fruitBasket);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Apples']);
+  })
+
+  test('Immutable Slice', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Apples', 'Strawberries'];
+    /* 
+     * Use the slice method to make a copy of the fruitBasket array
+     * containing only the FIRST three items of the array.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+
+    expect(updatedFruitBasket).toEqual(['Oranges', 'Bananas', 'Apples']);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Apples', 'Strawberries']);
+  })
+
+  test('Immutable Slice (until the end)', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Apples', 'Strawberries'];
+    /* 
+     * Use the slice method to make a copy of the fruitBasket array
+     * containing only the LAST two items ('Apples' and 'Strawberries').
+     * Notice that when slicing up to the end of the array, you don't need
+     * to provide the second parameter to the slice method.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const redFruits = fruitBasket;
+
+    expect(redFruits).toEqual(['Apples', 'Strawberries']);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Apples', 'Strawberries']);
+  })
+
+
+  test('Remove at specific index', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Sausages', 'Apples', 'Strawberries'];
+    /* 
+     * Use a combination of the spread operator and the slice method to make
+     * a copy of the fruitBasket array containing only the fruits
+     * (In other words, remove the 'sausages' entry at index 2).
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+
+    expect(updatedFruitBasket).toEqual(['Oranges', 'Bananas', 'Apples', 'Strawberries']);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Sausages', 'Apples', 'Strawberries']);
+  })
+
+  test('Update at specific index', () => {
+    const fruitBasket = ['Oranges', 'Bananas', 'Sausages', 'Apples', 'Strawberries'];
+    /* 
+     * Use a combination of the spread operator and the slice method to make
+     * a copy of the fruitBasket array updating the
+     * entry at index 2 (Sausages) for 'Blueberry'.
+     * 
+     * Assign the new array to the updatedFruitBasket constant.
+     */ 
+    const updatedFruitBasket = fruitBasket;
+
+    expect(updatedFruitBasket).toEqual(['Oranges', 'Bananas', 'Blueberry', 'Apples', 'Strawberries']);
+    expect(fruitBasket).toEqual(['Oranges', 'Bananas', 'Sausages', 'Apples', 'Strawberries']);
+  })
+});
+
+describe('Immutable manipulations for Objects', () => {
+  test('Object Shallow copy', () => {
+    const state = {
+      name: 'Jon Snow',
+      occupation: 'Lord Commander',
+      skills: [] // knows nothing...
+    }
+
+    /* 
+     * Use the spread operaror to make a copy of the 'state' object.
+     * 
+     * Assign the new object to the updatedState constant.
+     */ 
+
+    const updatedState = state;
+    expect(updatedState).toEqual({name: 'Jon Snow', occupation: 'Lord Commander', skills: []});
+    expect(updatedState).not.toBe(state);
+  })
+
+  test('Immutable update', () => {
+    const state = {
+      name: 'Jon Snow',
+      occupation: 'Lord Commander',
+      skills: [] // knows nothing...
+    }
+
+    /* 
+     * Use the spread operaror to make a copy of the 'state' object
+     * while at the same updating the value of the 'occupation' key
+     * to 'King in the North'.
+     * 
+     * Assign the new object to the updatedState constant.
+     */ 
+
+    const updatedState = state;
+    expect(updatedState).toEqual({name: 'Jon Snow', occupation: 'King in the North', skills: []});
+    expect(updatedState).not.toBe(state);
+  })
+
+  test('Immutable deep update', () => {
+    const state = {
+      name: 'Jon Snow',
+      occupation: 'Lord Commander',
+      skills: [] // knows nothing...
+    }
+
+    /* 
+     * Use the spread operaror to make a copy of the 'state' object
+     * while at the same updating the value of the 'occupation' key
+     * to 'King in the North' and adding a new item "Fighting"
+     * to the skills array.
+     * 
+     * Be aware that the spread operator makes shallow copies,
+     * so if you try to add an item to the `skills` array directly,
+     * you'll end up changing the original one.
+     * 
+     * Assign the new object to the updatedState constant.
+     */ 
+
+    const updatedState = state;
+
+    expect(updatedState).toEqual({
+      name: 'Jon Snow', occupation: 'King in the North', skills: ['Fighting']
+    });
+    expect(updatedState.skills).not.toBe(state.skills);
+    expect(updatedState).not.toBe(state);
+  })
+});
+
+describe('Bonus', () => {
+  test('Immutable maniulation using dynamic keys', () => {
+    const ownedCars = {
+      ferrari: true,
+      porsche: false,
+      shelby: false,
+    };
+
+    const recentlyBoughtCar = 'porsche';
+
+    /* 
+     * Use the spread operaror to make a copy of the 'ownedCars' object
+     * while at the same updating to true the value whose key is stored
+     * in the 'recentlyBoughtCar' variable.
+     * 
+     * Assign the new object to the updatedOwnedCars constant.
+     */ 
+
+    const updatedOwnedCars = ownedCars;
+    expect(updatedOwnedCars).toEqual({
+      ferrari: true, porsche: true, shelby: false
+    });
+    expect(updatedOwnedCars).not.toBe(ownedCars);
+  })
+});
